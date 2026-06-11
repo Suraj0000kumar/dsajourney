4b3ab713a946f62da896af7dedfe4a1114bb35c5
Suraj0000kumar <surajkantkumar7412@gmail.com>
2026-06-12 01:13:00 +0530

    new solution added

--- day1.java
+++ day1.java
@@ -1,16 +1,29 @@
-import java.util.*;
-
 class Day1 
 {
  public static void main(String args[]) 
   { 
    Scanner sc=new Scanner(System.in);
-   int N = sc.nextInt();
+   int Q = sc.nextInt();
+   int arr[][] =new int[Q][3];
+   for(int i=0;i<Q;i++)
+   {
+     for(int j=0;j<3;j++)
+      {
+       arr [i][j]=sc.nextInt();
+       System.out.print(" ");
+      }
+     System.out.println();
+   }
+     //for(int i=0;i<=10;i++){System.out.println(N+" *  "+ i+" = "+(N*i));}
    
-   for(int i=0;i<=10;i++)
+   for(int i=0;i<Q;i++)
     {
-      System.out.println(N+" *  "+ i+" = "+(N*i));
+      for(int j=0;j<arr[i][2];i++)
+       {
+         System.out.print(arr[i][0]+((math.pow(arr[i][1],j)).b));
+         System.out.print(" ");
+       } 
+      System.out.println();
     }
-   sc.close();
   }
-}		 
+}
